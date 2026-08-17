daa9df9203c96064ab23db840b2b93db075d7022
hboruah <148325082+hboruah@users.noreply.github.com>
2026-08-17 22:09:36 +0530

    Update Application.xaml.cs

--- Application/FileConverter/Application.xaml.cs
+++ Application/FileConverter/Application.xaml.cs
@@ -39,7 +39,7 @@ namespace FileConverter
                                                       {
                                                           Major = 2,
                                                           Minor = 2,
-                                                          Patch = 0,
+                                                          Patch = 1,
                                                       };
 
         private bool needToRunConversionThread;
