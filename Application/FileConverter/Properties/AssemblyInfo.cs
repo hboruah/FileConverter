222ff241cf00e4c9a4c1c2bc4f9250add3ed8424
hboruah <148325082+hboruah@users.noreply.github.com>
2026-08-19 00:32:25 +0530

    Keep AssemblyInfo unchanged except version 2.3

--- Application/FileConverter/Properties/AssemblyInfo.cs
+++ Application/FileConverter/Properties/AssemblyInfo.cs
@@ -4,7 +4,7 @@ using System.Windows;
 
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
-// associated with the assembly.
+// associated with an assembly.
 [assembly: AssemblyTitle("FileConverter")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
@@ -21,7 +21,8 @@ using System.Windows;
 // In order to begin building localizable applications, set 
 // <UICulture>CultureYouAreCodingWith</UICulture> in your .csproj file
 // inside a <PropertyGroup>.  For example, if you are using US english
-// in your source files, set the <UICulture> to en-US.  Then uncomment the NeutralResourcesLanguage attribute below. Update the "en-US" in the line below to match the UICulture setting in the project file.
+// in your source files, set the <UICulture> to en-US.  Then uncomment
+// the NeutralResourcesLanguage attribute below. Update the "en-US" in the line below to match the UICulture setting in the project file.
 
 //[assembly: NeutralResourcesLanguage("en-US", UltimateResourceFallbackLocation.Satellite)]
 
